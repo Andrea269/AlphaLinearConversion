4fd8eaed76889cf2a7d2b4b2af827fd630172748
Andrea269 <nash25032710>
2019-05-14 17:34:49 +0200

    Crumbling-Project

--- Crumbling-Project/main.c
+++ Crumbling-Project/main.c
@@ -5,24 +5,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct ListNode;
-struct InfoNode;
-struct Term;
-
 enum Bool {
     True, False
 };
+
+struct List;
+struct Term;
 /*********************************************************************************/
 
+struct Var {
+    int dummy;
+};
 struct Node {
     struct Term *content;
     enum Bool copying;
-    struct Node *prev;
+    struct List *prev;//option
 };
 struct Env {
     struct Node *node1;
     struct Node *node2;
-    struct ListNode *env;
+    struct List *env;//option
 };
 struct Crumblep {
     struct Term *term;
@@ -30,85 +32,86 @@ struct Crumblep {
 };
 /*********************************************************************************/
 struct NodeVar {
-    struct InfoNode *infoNode;
-    int dummy;
-    struct NodeLam *binder;
+    struct Var *var;
 };
 struct NodeApp {
-    struct Term *left;
-    struct Term *right;
-    struct InfoNode *infoNode;
+    struct Term *term1;
+    struct Term *term2;
 };
 struct NodeLam {
-    struct NodeVar *left;
-    struct Crumblep *right;
-    struct InfoNode *infoNode;
+    struct NodeVar *var;
+    struct Crumblep *crumblep;
 };
 struct NodeIf {
     struct Term *term;
-    struct Crumblep *ifThen;
-    struct Crumblep *ifElse;
-    struct InfoNode *infoNode;
+    struct Crumblep *crumblepThen;
+    struct Crumblep *crumblepElse;
 };
 struct NodeShared {
     struct Node *node;
 };
 
-struct TypeNode {
-    int label;
-    union {
-        struct NodeVar var;
-        struct NodeApp app;
-        struct NodeLam lam;
-        struct NodeIf nodeIf;
-        struct NodeShared shared;
-    } content;
+enum TypeTerm {
+    Bool, NodeVar, NodeApp, NodeLam, NodeIf, NodeShared
 };
-/*********************************************************************************/
-
-struct TypeList {
-    int label;
-    union {
-        struct TypeNode typeNode;
-        struct Node node;
-        struct Env env;
-    } content;
-};
-struct ListNode {
-    struct ListNode *head;
-    struct TypeList *node;
-    struct ListNode *next;
-    int count;
-};
-struct IndirectEdge {
-    struct TypeNode *left;
-    struct TypeNode *right;
-    int isAlive;//1 se è vivo 0 altrimenti
-};
-struct ListIndirectEdge {
-    struct ListIndirectEdge *head;
-    struct IndirectEdge *edge;
-    struct ListIndirectEdge *next;
-    int count;
-};
-
-struct InfoNode {
-    struct ListNode *parentNodes;
-    struct TypeNode *canonic;
-    int isAlive;//1 se è vivo 0 altrimenti
-    struct ListIndirectEdge *indirectEdge;
-};
-
-
 struct Term {
-    int label;
+    enum TypeTerm label;
     union {
         enum Bool bool;
-        struct TypeNode node;
+        struct NodeVar *var;
+        struct NodeApp *app;
+        struct NodeLam *lam;
+        struct NodeIf *nodeIf;
+        struct NodeShared *shared;
     } content;
 };
 /*********************************************************************************/
+enum TypeElementList {
+    Term, Node, Env
+};
+struct ElementList{
+    enum TypeElementList  label;
+    union {
+        struct Term *term;
+        struct Node *node;
+        struct Env *env;
+    } content;
+};
 
+struct List {
+    struct List *head;
+    struct ElementList *element;
+    struct List *next;
+    int count;
+};
+
+/**
+ * Inizializza l'elemento in coda alla lista passata in input
+ * @param listNode puntatore alla coda della lista
+ * @param head della lista
+ */
+void InitList(struct List *listNode, struct List *head) {
+    listNode->head = head;
+    listNode->element = malloc(sizeof(struct ElementList));
+    listNode->next = malloc(sizeof(struct List));
+    listNode->count = 0;
+};
+
+struct List *PushToList(struct List *list, struct ElementList *prtElement) {
+    list->element = prtElement;
+    InitList(list->next, list->head);
+    list->next->count = list->count + 1;
+    return list->next;
+};
+
+void PushListNode(struct Node *node, struct Node *prev) {
+    struct ElementList *element=malloc(sizeof(struct ElementList));
+    element->label=Node;
+    element->content.node=malloc(sizeof(struct Node));
+    element->content.node=prev;
+    node->prev = PushToList(node->prev, element);
+}
+/*********************************************************************************/
 
 struct Node *mk_node(struct Term *content) {
     struct Node *node = malloc(sizeof(struct Node));
@@ -118,29 +121,23 @@ struct Node *mk_node(struct Term *content) {
     return node;
 }
 
-void evalOpen(struct Node *n) {
 
-}
-
-void push(struct Node *node, struct Node *prev) {
-    node->prev = prev;
-}
 
 struct Node *at(struct Node *node, struct Term *content, struct Env *env) {
     node->content = content;
     if (env == NULL)
         return node;
     else if (env->node1 != NULL && env->node2 != NULL) {
-        push(env->node1, node);
+        PushListNode(env->node1, node);
         return env->node2;
     }
-    exit(0); //todo modificare
+    exit(1);
 }
 
 struct Node *CopyingNode(struct Node *node, struct Node *cNode, struct Node *(f)()) {
     struct Term *saved = node->content;
-    struct Term *shared = malloc(sizeof(struct NodeShared));
-    shared->content.node.content.shared.node = cNode;
+    struct Term *shared = malloc(sizeof(struct Term));
+    shared->content.shared->node = cNode;
     node->content = shared;
     node->copying = True;
     struct Node *res = f();
@@ -150,9 +147,8 @@ struct Node *CopyingNode(struct Node *node, struct Node *cNode, struct Node *(f)
 }
 
 struct Term *Dummy() {
-    struct Term *var = malloc(sizeof(struct NodeVar));
-    var->content.node.content.var.dummy = 0;
-    var->content.node.content.var.binder = NULL;
+    struct Term *var = malloc(sizeof(struct Term));
+    var->content.var->var->dummy = 0;
     return var;
 }
 
@@ -161,8 +157,30 @@ struct Node *Iota(struct Crumblep e) {
     return at(star, e.term, e.env);
 }
 
+/*********************************************************************************/
+void evalOpen(struct Node *n) {
+
+}
+
+
+
+
+
+
+/*********************************************************************************/
 int main() {
     printf("Hello, World!\n");
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
 
